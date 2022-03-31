8df6d49b744e9ccdedcbc78a157868fabb809d29
LiliGC <liliana.garmendia.c@gmail.com>
2022-03-30 21:55:59 -0300

    Agregue metodo para ver productos

--- clase_producto.py
+++ clase_producto.py
@@ -4,3 +4,12 @@ class Producto:
         self.nombre = nombre
         self.tipo = tipo
         self.precio = precio
+   #Método para obtener en pantalla un string con los datos del producto 
+    def __str__(self):
+        return '{:<30}{:<15}{:<15}'.format(self.nombre, self.tipo, self.precio)
+    def mostrar_productos():
+        print("Los productos en nuestra bodega son los siguientes: \n\n"  '{:<30}{:<15}{:<15}'.format("Nombre", "Tipo", "Precio"))
+        print(("*"*70))
+        for key in productos:
+            print(productos[key])
+            input()
