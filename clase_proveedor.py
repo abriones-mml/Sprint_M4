5e4e65e21d8c83a8999dc572d739c454274295e9
Alberto <aabrionesf@gmail.com>
2022-04-04 06:59:38 -0300

    todo lo solicitado en pdf ha sido incorporado

--- clase_proveedor.py
+++ clase_proveedor.py
@@ -5,11 +5,15 @@ class Proveedor:
         self.nombre = nombre
         self.tipo_producto = tipo_producto
         
-    def inscripcion_en_bodega(self, bodega):
-        pass
+    def inscripcion_en_bodega(self, bodega, dict_proveedores):
+        bodega.proveedores[self.id] = dict_proveedores[self.id]
+        print(f"El proveedor {self.nombre} ha sido inscrito en {bodega.nombre}")
     
-    def modificar_tipo(self):
-        pass
+    def modificar_tipo(self, tipo_anterior):
+        t = input(f"Ingrese el nuevo tipo de producto del proveedor {self.nombre}:  ").title()
+        self.tipo_producto = t
+        print(f"El tipo de producto del proveedor {self.nombre} ha cambiado de {tipo_anterior} a {self.tipo_producto}.")
+
     
     def __str__(self):
         return '{:<5}{:<15}{:<20}'.format(self.id, self.nombre, self.tipo_producto)
